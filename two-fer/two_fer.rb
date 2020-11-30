c62fe269b23718cca35d01cb3cf0ac6d9d5f898c
adonai-luque <akluqueprieto@gmail.com>
2020-11-30 16:53:47 -0500

    two-fer solved

--- two-fer/two_fer.rb
+++ two-fer/two_fer.rb
@@ -1,8 +1,8 @@
-=begin
-Write your code for the 'Two Fer' exercise in this file. Make the tests in
-`two_fer_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/two-fer` directory.
-=end
-
+# An object of the class TwoFer returns a customized message when
+# a name is passed as an argument to its class method "two_fer".
+# If a name is not passed it takes a default value of "you"
+class TwoFer
+  def self.two_fer(name = 'you')
+    "One for #{name}, one for me."
+  end
+end
