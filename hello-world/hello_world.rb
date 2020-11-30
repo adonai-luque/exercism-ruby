810b5cb782e3e754aa8d958867a6d829389fdc25
adonai-luque <akluqueprieto@gmail.com>
2020-11-30 16:00:14 -0500

    hello-world solved

--- hello-world/hello_world.rb
+++ hello-world/hello_world.rb
@@ -1,8 +1,7 @@
-=begin
-Write your code for the 'Hello World' exercise in this file. Make the tests in
-`hello_world_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/hello-world` directory.
-=end
-
+# An object of the class HelloWorld returns a string "Hello, World!"
+# when its class method "hello" is called
+class HelloWorld
+  def self.hello
+    'Hello, World!'
+  end
+end
