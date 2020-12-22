e25bdb2cbfcbdbc3c1eda4fc819b8a998df733f7
adonai-luque <akluqueprieto@gmail.com>
2020-12-22 16:36:03 -0500

    resistor-color-duo 3rd iteration

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,11 +1,12 @@
-# class ResistorColorDuo has a constant COLORS that stores colors in an array and a class method value
+# class ResistorColorDuo has a constant 'BAND_COLORS' that stores colors in an array and a class method 'value'
+# This class was created in order to find the numeric value of a resistor given its band colors
 class ResistorColorDuo
   # The index of the color in the array matches its color code
-  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+  BAND_COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
   
-  # The class method value returns a numeric value of the resistor based on its first two colors
+  # The class method 'value' returns a numeric value of the resistor based on its first two band colors
   def self.value(colors)
-    10 * COLORS.index(colors[0]) + COLORS.index(colors[1])
+    10 * BAND_COLORS.index(colors[0]) + BAND_COLORS.index(colors[1])
   end
 end
     
