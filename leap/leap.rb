4de385fa51c00d5ff3502a06fa3a6ecf701995e6
adonai-luque <akluqueprieto@gmail.com>
2020-12-22 17:31:43 -0500

    leap solved

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,13 +1,12 @@
+# class 'Year' was implemented in order to find if a year is a leap year or not
 class Year
+  # The class method 'leap?' returns true if the year passed as an argument is a leap year
+  # or false otherwise
   def self.leap?(anno)
-    if (anno % 400).zero?
-      return true
-    elsif (anno % 100).zero?
-      return false
-    elsif (anno % 4).zero?
-      return true
-    else
-      return false
-    end
+    return true if (anno % 400).zero?
+    return false if (anno % 100).zero?
+    return true if (anno % 4).zero?
+
+    false
   end
-  
+end
