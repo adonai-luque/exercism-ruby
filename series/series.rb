ab061b068f1bf7ac7ff743bdef66b005ddab7539
adonai-luque <akluqueprieto@gmail.com>
2021-08-09 01:27:46 -0500

    series first iteration

--- series/series.rb
+++ series/series.rb
@@ -1,8 +1,22 @@
-=begin
-Write your code for the 'Series' exercise in this file. Make the tests in
-`series_test.rb` pass.
+class Series
+  private
 
-To get started with TDD, see the `README.md` file in your
-`ruby/series` directory.
-=end
+  def initialize(digits)
+    @digits = digits
+  end
 
+  public
+
+  attr_reader :digits
+
+  def digits_length
+    digits.length
+  end
+
+  def slices(number_of_digits)
+    raise ArgumentError if number_of_digits > digits_length
+      
+    last_start = digits_length - number_of_digits
+    (0..last_start).map { |n| digits[n, number_of_digits] }
+  end
+end
