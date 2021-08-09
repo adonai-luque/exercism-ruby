e91a413931ec7e95351d7e8f02177b67482ed18c
adonai-luque <akluqueprieto@gmail.com>
2021-08-08 22:59:24 -0500

    high-scores second iteration

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -1,12 +1,17 @@
 # class 'HighScores' was created in order to provide insights into a list of scores
 class HighScores
-  attr_reader :scores
+  
+  private
 
   # An object is initialized passing an array of scores as a parameter
-  # Those scores can be accessed calling the reader method 'scores'
   def initialize(scores)
     @scores = scores
   end
+
+  public
+
+  # The scores can be accessed calling the reader method 'scores'
+  attr_reader :scores
 
   # This method returns the latest registered score
   def latest
@@ -15,16 +20,13 @@ class HighScores
 
   # This method returns the best score. The maximum score of the list
   def personal_best
-    scores.max
+    personal_top_three.first
   end
 
   # This method returns the top three scores. If there aren't 3 scores in the list
   # it returns as much scores as it can get
   def personal_top_three
-    scores
-      .sort
-      .reverse
-      .slice(0, [scores.length, 3].min)
+    scores.max([scores.length, 3].min)      
   end
 
   # This method evaluates if the latest score registered is also the best score
